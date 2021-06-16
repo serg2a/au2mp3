86e66eb8dd9576f79367f8bfb8c39c009c6efeb4
Sergey Alexndrovich <serg2ak@ya.ru>
2021-06-16 22:45:02 +1000

    add hiden ffmpeg

--- main.c
+++ main.c
@@ -4,39 +4,44 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+
 #define FORK_MAX 12
+#define NEWFORMAT ".mp3"
+
 
 
 int main(int argc, char *argv[]){
-	int status;
-	int run = 0;
+    int status;
+    int run = 0;
     char tmp_str[4096];
+    pid_t pid;
 
-	while(argc-1 && *argv)
-	{
-		argv++;
-		if (run <= FORK_MAX-1)
-			if(!fork())
-			{
-                printf("%s\n", *argv);
+    while(argc-1 && *argv)
+    {
+        argv++;
+        if (run <= FORK_MAX-1)
+        {
+            if((pid = fork()) < 0)
+                exit(1);
+            if(!pid)
+            {
                 strcat(tmp_str, *argv);
-                strcat(tmp_str, ".mp3");
+                strcat(tmp_str, NEWFORMAT);
                 printf("run convert: %s\n", *argv);
-				execlp("ffmpeg", "ffmpeg", "-i", *argv, tmp_str, (char*)NULL);
+
+                execlp("ffmpeg", "ffmpeg", "-hide_banner", "-loglevel", "-8", "-i", *argv, tmp_str, (char*)NULL);
+
                 printf("complete convert: %s\n", tmp_str);
-				exit(0);
+                exit(0);
+            }
+            else
+            {
+                run++;
+                argc--;
 			}
-		    else
-			{
-				run++;
-				argc--;
-			}
-		else
-		{
-			wait(&status);
-			run--;
-			printf("Exit children\n");
-		}
-	}
-	exit(0);
+        }
+        else if(wait(&status))
+            run--;
+    }
+    exit(0);
 }
